8714050ba9b8f04f456f7de1e4aaf4149b2a0b0c
Stewart McMillan <stewartmcm@gmail.com>
2016-06-03 02:32:18 -0500

    refactored mainActivity and Constants

--- PredictEV/app/src/main/java/com/stewartmcm/android/evclarity/activities/Constants.java
+++ PredictEV/app/src/main/java/com/stewartmcm/android/evclarity/activities/Constants.java
@@ -1,9 +1,5 @@
 package com.stewartmcm.android.evclarity.activities;
 
-import android.content.Context;
-import android.content.res.Resources;
-
-import com.example.android.predictev.R;
 import com.google.android.gms.location.DetectedActivity;
 
 /**
@@ -15,10 +11,6 @@ public final class Constants {
     }
 
     public static final String PACKAGE_NAME = "com.example.android.predictev";
-
-    public static final String BROADCAST_ACTION = PACKAGE_NAME + ".BROADCAST_ACTION";
-
-    public static final String ACTIVITY_EXTRA = PACKAGE_NAME + ".ACTIVITY_EXTRA";
 
     public static final String SHARED_PREFERENCES_NAME = PACKAGE_NAME + ".SHARED_PREFERENCES";
 
@@ -62,31 +54,4 @@ public final class Constants {
             DetectedActivity.TILTING,
             DetectedActivity.UNKNOWN
     };
-
-    /**
-     * Returns a human readable String corresponding to a detected activity type.
-     */
-    public static String getActivityString(Context context, int detectedActivityType) {
-        Resources resources = context.getResources();
-        switch (detectedActivityType) {
-            case DetectedActivity.IN_VEHICLE:
-                return resources.getString(R.string.in_vehicle);
-            case DetectedActivity.ON_BICYCLE:
-                return resources.getString(R.string.on_bicycle);
-            case DetectedActivity.ON_FOOT:
-                return resources.getString(R.string.on_foot);
-            case DetectedActivity.RUNNING:
-                return resources.getString(R.string.running);
-            case DetectedActivity.STILL:
-                return resources.getString(R.string.still);
-            case DetectedActivity.TILTING:
-                return resources.getString(R.string.tilting);
-            case DetectedActivity.UNKNOWN:
-                return resources.getString(R.string.unknown);
-            case DetectedActivity.WALKING:
-                return resources.getString(R.string.walking);
-            default:
-                return resources.getString(R.string.unidentifiable_activity, detectedActivityType);
-        }
-    }
 }
