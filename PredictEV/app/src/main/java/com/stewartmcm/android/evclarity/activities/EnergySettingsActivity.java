3cc32d9b51cf03dd1c5aa8067bbd6893760e6e06
Stewart McMillan <stewartmcm@gmail.com>
2016-06-14 08:09:52 -0400

    moved api key to gradle.properties

--- PredictEV/app/src/main/java/com/stewartmcm/android/evclarity/activities/EnergySettingsActivity.java
+++ PredictEV/app/src/main/java/com/stewartmcm/android/evclarity/activities/EnergySettingsActivity.java
@@ -15,6 +15,7 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.android.predictev.BuildConfig;
 import com.example.android.predictev.R;
 import com.stewartmcm.android.evclarity.models.Utility;
 import com.stewartmcm.android.evclarity.models.UtilityArray;
@@ -129,7 +130,7 @@ public class EnergySettingsActivity extends AppCompatActivity {
 
         Call<UtilityArray> call = null;
 
-        call = mService.getElectricityProviders("vIp4VQcx5zLfEr7Mi61aGd2vjIDpBpIqQRRQCoWt", latString, lonString);
+        call = mService.getElectricityProviders(BuildConfig.THE_NREL_API_TOKEN, latString, lonString);
 
         if (call != null) {
             call.enqueue(new Callback<UtilityArray>() {
